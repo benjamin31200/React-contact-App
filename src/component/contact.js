565e24bc1edcb1428734c847969949aaafa21de3
benjamin31200 <benjamindeneux@hotmail.com>
2022-06-16 11:36:54 +0200

    ajout des props

--- src/component/contact.js
+++ src/component/contact.js
@@ -6,12 +6,12 @@ const Contact = props => {
     return (
       <div className="friend-item">
         {/* replace the values by a jsx expression */}
-        <h3>props.name</h3>
-        <h4>props.email</h4>
-        <h4>props.phone</h4>
+        <h3>{props.name}</h3>
+        <h4>{props.email}</h4>
+        <h4>{props.phone}</h4>
         {/* show the button only if isDeletable is true */}
         <div>
-            { isDeletable ? <button>Delete</button> : null}
+            { isDeletable ? <button>Delete</button> : null }
         </div>
       </div>
     );
